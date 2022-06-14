a632f7647a0ca6b5148aafff5082ab391a5bc8da
Mukhammadali <mukhammadalime@gmail.com>
2022-06-14 23:26:12 +0900

    implemented addToCart and removeFromCart with redux toolkit

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,14 +1,25 @@
-import classes from './CartItem.module.css';
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch();
+
+  const removeItem = (id) => {
+    dispatch(cartActions.removeItemFromCart({ id }));
+  };
+
+  const addItem = (item) => {
+    dispatch(cartActions.addItemToCart({ id, title, price }));
+  };
 
   return (
     <li className={classes.item}>
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
+          ${total.toFixed(2)}{" "}
           <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
@@ -17,8 +28,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItem.bind(null, id)}>-</button>
+          <button onClick={addItem.bind(null, props.item)}>+</button>
         </div>
       </div>
     </li>
