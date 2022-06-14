d3ee55b193e6edd4cecf53881bc8fdd56d1ddb6d
Mukhammadali <mukhammadalime@gmail.com>
2022-06-15 05:43:05 +0900

    added redux thunk (middleware)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -15,51 +15,13 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-
-      const response = await fetch(
-        `https://react-food-by-mk-default-rtdb.firebaseio.com/cart.json`,
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed!");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sending cart data successfully!",
-        })
-      );
-    };
-
     if (isInitial) {
       isInitial = false;
       return;
     }
 
-    sendCartData().catch((err) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed",
-        })
-      );
-    });
-  }, [cart, dispatch]);
+    dispatch(sendCartData(cart));
+  }, [dispatch, cart]);
 
   return (
     <>
--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import { Provider } from "react-redux";
 import store from "./store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+// Old version
+// import { applyMiddleware } from "@reduxjs/toolkit";
+// import { thunk } from "redux-thunk";
+// const store = creaseStore(store, applyMiddleware(thunk))
+
 root.render(
   <Provider store={store}>
     <App />
